2354af158cb08eea17de922fecd827c9284b9441
SergeyTimofeev1 <timofeevbest@yandex.ru>
2022-12-22 14:33:17 +0400

    feat: add animations

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,26 +1,37 @@
 import React, { useContext } from 'react';
+import { CSSTransition } from 'react-transition-group'
 import { AlertContext } from '../context/alert/alertContext';
 
 const Alert = () => {
 
   const { alert, hide } = useContext(AlertContext)
 
-  if (!alert.visible) return null
-
   return (
-    <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-      <strong>Внимание!</strong>
-      &nbsp;{alert.text}
-      <button
-        type='button'
-        className='alert-btn'
-        aria-label='Close'
-        onClick={hide}
-        data-dismiss="alert"
-      >
-        <span aria-hidden='true'>&times;</span>
-      </button>
-    </div>
+    <CSSTransition
+      in={alert.visible}
+      timeout={{
+        enter: 500,
+        exit: 350
+      }}
+      classNames={"alert"}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div div className={`alert alert-${alert.type || 'warning'} alert-dismissible`
+      }>
+        <strong>Внимание!</strong>
+        &nbsp; {alert.text}
+        <button
+          type='button'
+          className='alert-btn'
+          aria-label='Close'
+          onClick={hide}
+          data-dismiss="alert"
+        >
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div >
+    </CSSTransition>
   )
 }
 
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 const Notes = ({ notes, onRemove }) => {
   return (
-    <ul className="list-group">
+    <TransitionGroup component='ul' className="list-group">
       {notes.map(note =>
-        <li
-          className="list-group-item note"
-          key={note.id}
-        >
-          <strong>{note.title}</strong>
-          <small className="note-date">{note.date}</small>
-          <button
-            type="button"
-            className="btn btn-danger btn-sm"
-            onClick={() => onRemove(note.id)}
+        <CSSTransition key={note.id} classNames={'note'} timeout={500}>
+          <li
+            className="list-group-item note"
           >
-            &times;
-          </button>
-        </li>
+            <strong>{note.title}</strong>
+            <small className="note-date">{note.date}</small>
+            <button
+              type="button"
+              className="btn btn-danger btn-sm"
+              onClick={() => onRemove(note.id)}
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
       )}
-    </ul>
+    </TransitionGroup>
   );
 }
 
